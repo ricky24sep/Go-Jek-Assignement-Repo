8f036b3876ff575567e615c28c932d11d3e329e6
rikky.chavan <jaidev_chavan_rikky_kumar@network.lilly.com>
2020-02-21 14:41:55 +0530

    Project added

---
+++ Go-Jek-Assignment/Go-Jek-AssignmentTests/Go_Jek_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Go_Jek_AssignmentTests.swift
+//  Go-Jek-AssignmentTests
+//
+//  Created by Rikky Chavhan on 21/02/20.
+//  Copyright © 2020 L&T. All rights reserved.
+//
+
+import XCTest
+@testable import Go_Jek_Assignment
+
+class Go_Jek_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
